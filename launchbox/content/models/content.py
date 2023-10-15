ea320349bfd7257bc4469a05edc37452d5ad7348
launchat <t.endo@launchat.jp>
2023-10-15 19:11:29 +0900

    Update content models

--- launchbox/content/models/content.py
+++ launchbox/content/models/content.py
@@ -7,36 +7,6 @@ from launchbox.core.models import TimeStampedMixin, UUIDPrimaryKeyMixin
 from .service import Service
 
 
-class Content(TimeStampedMixin, UUIDPrimaryKeyMixin, models.Model):
-    body_html = models.TextField(
-        _('content body html'),
-        blank=True,
-        null=True,
-        default=None,
-    )
-    is_draft = models.BooleanField(_('draft'), default=True)
-    service = models.ForeignKey(
-        Service,
-        on_delete=models.CASCADE,
-        verbose_name=_('service'),
-    )
-    owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.PROTECT,
-        related_name='+',
-        verbose_name=_('owner'),
-    )
-
-    class Meta:
-        db_table = 'launchbox_content'
-        verbose_name = _('content')
-        verbose_name_plural = _('contents')
-        ordering = ('-created_at',)
-
-    def __str__(self) -> str:
-        return '{}'.format((self.body_html or '')[0:30]) or '{}'.format(_('empty page'))
-
-
 class ContentTheme(TimeStampedMixin, UUIDPrimaryKeyMixin, models.Model):
     name = models.CharField(_('theme name'), max_length=32)
     css = models.TextField(_('theme css'), blank=True, null=True, default=None)
@@ -61,6 +31,51 @@ class ContentTheme(TimeStampedMixin, UUIDPrimaryKeyMixin, models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Content(TimeStampedMixin, UUIDPrimaryKeyMixin, models.Model):
+    title = models.CharField(
+        _('content title'),
+        max_length=32,
+        blank=True,
+        null=True,
+        default=None,
+    )
+    body_html = models.TextField(
+        _('content body html'),
+        blank=True,
+        null=True,
+        default=None,
+    )
+    is_draft = models.BooleanField(_('draft'), default=True)
+    theme = models.ForeignKey(
+        ContentTheme,
+        on_delete=models.SET_NULL,
+        verbose_name=_('theme'),
+        blank=True,
+        null=True,
+        default=None,
+    )
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        verbose_name=_('service'),
+    )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='+',
+        verbose_name=_('owner'),
+    )
+
+    class Meta:
+        db_table = 'launchbox_content'
+        verbose_name = _('content')
+        verbose_name_plural = _('contents')
+        ordering = ('-created_at',)
+
+    def __str__(self) -> str:
+        return self.title or '{}'.format((self.body_html or '')[0:30]) or '{}'.format(_('empty page'))
 
 
 # class ContentEdit(TimeStampedMixin, UUIDPrimaryKeyMixin, models.Model):
